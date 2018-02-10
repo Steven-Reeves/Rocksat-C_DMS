d8e71b7cd5a302303a1299618e3ca8e1876a92a0
Steven Reeves <stevenreeves15@gmail.com>
2018-02-10 03:37:27 +0000

    Added test code and files

---
+++ Python code/Helloworld.py
@@ -0,0 +1,37 @@
+# initial sandbox for writing to and from stuff
+# \ characters not working
+import time
+import csv
+from datetime import datetime
+
+
+print("Hello!")
+
+
+
+log = open("log.txt", "w")
+
+log.write("*************Initating log*****************\n")
+log.write(str(datetime.now())+ "\n")
+log.write("Looping to create data in two dimentional array\n")
+myData = [["Time", "Count", "Col 3", "Col 4"]]
+
+for x in range(1,10):
+    myData.append([str(datetime.now()),x,"filler","also"])
+    log.write("Data Written!\n")
+    time.sleep(1)   
+
+log.write("Fake data initialized\n")
+log.write("This is where to start a .csv file\n")
+with open("data.csv", "w") as d:
+    writer = csv.writer(d)
+    writer.writerows(myData)
+
+log.write("Data written to .csv!\n")
+
+print("Closing log")
+log.close()
+
+
+
+      
