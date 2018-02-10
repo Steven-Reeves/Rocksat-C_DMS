92236aa86d2fa9ac45910a44cc784b6da584aca3
Steven Reeves <steven@digieffects.com>
2018-02-09 20:37:22 -0800

    Added some spelling corrections

--- Python code/Helloworld.py
+++ Python code/Helloworld.py
@@ -13,11 +13,11 @@ log = open("log.txt", "w")
 
 log.write("*************Initating log*****************\n")
 log.write(str(datetime.now())+ "\n")
-log.write("Looping to create data in two dimentional array\n")
+log.write("Looping to create data in two dimensional array\n")
 myData = [["Time", "Count", "Col 3", "Col 4"]]
 
 for x in range(1,10):
-    myData.append([str(datetime.now()),x,"filler","also"])
+    myData.append([str(datetime.now()),x,"filler","example"])
     log.write("Data Written!\n")
     time.sleep(1)   
 
