313abf8175d14f81409922a5fd4dc61fc22552c8
Steven Reeves <steven@digieffects.com>
2018-03-04 21:22:28 -0800

    Adding further testing for serial_test

--- Arduino code/Primary_Arduino_Tests/serial_test/serial_test.ino
+++ Arduino code/Primary_Arduino_Tests/serial_test/serial_test.ino
@@ -1,6 +1,8 @@
-//char dataString[50] = {0};
-//int a =0;
-byte number = 0; 
+char dataString[50];
+int iter =0;
+char a = 0;
+byte number = 0;
+
 
 void setup() {
 Serial.begin(9600);              //Starting serial communication
@@ -10,8 +12,14 @@ void loop() {
 
   if (Serial.available()) {     // Check if message is recieved
       number = Serial.read();
-      Serial.print("character recieved: ");
+      a = number;
+      Serial.print("character recieved as DEC: ");
       Serial.println(number, DEC);
+      Serial.print("character recieved as CHAR: ");
+      Serial.println(a);
+      dataString[iter++] = a;
+      Serial.print("Full word so far: ");
+      Serial.println(dataString);
   }
   //a++;                          // a value increase every loop
   //sprintf(dataString,"%02X",a); // convert a value to hexa 
