865ae366fd7c450a414743deec0a51d83f930d74
Andy <andyhorn@users.noreply.github.com>
2018-04-05 16:13:36 -0700

    Improved timer_test

--- Python code/Pi/thread_func.py
+++ Python code/Pi/thread_func.py
@@ -12,19 +12,26 @@ def print_time(title='default', cycles=3, delay=1):
 		print(title + ": " + time.ctime(time.time()))
 		time.sleep(delay)
                 
-def alarm(run, count):
+def alarm(count, run):
     print("Process timed out at count %i!") % count
+    del run[0] # acts like pass-by-reference, removes only value in list
+    # essentially turning run into a False boolean
                 
 def timer_test():
     count = 0
-    run = True
+    run = [1] # allows 'alarm' method to impact this method
+    # Python normally uses pass-by-value, passing a list will
+    # simulate passing by reference.
     while run and count < 5:
         print("Count: %i") % count
-        timer = Timer(3, alarm, (run, count))
+        timer = Timer(3, alarm, (count, run))
         timer.start()
         time.sleep(count)
         timer.cancel()
         print("Count: %i completed.") % count
-        count += 1
+        if run:
+            count += 1
     if not run:
         print("Process ended at count " + str(count))
+    if count == 5:
+        print("Count reached 5")
