04acdac6c531a4077e094d88421600919196c7b7
Andy <andyjhorn@gmail.com>
2018-04-06 05:09:31 +0000

    Tested throwing exceptions from Timer, does not work. Timer creates a new thread, so exception handling will not catch exceptions raised in the method called by Timer.

--- Python code/Pi/main.py
+++ Python code/Pi/main.py
@@ -9,7 +9,7 @@ from thread_func import print_time, timer_test
 
 dt = DataThread()
 #dt.add_thread(read_serial, port="/dev/ttyAMA0", baudrate=9600, filename="Test-1")
-dt.add_thread(print_time,'Thread-1', 5)
+# dt.add_thread(print_time,'Thread-1', 5)
 dt.add_thread(print_time,'Thread-2', 5, 2)
-dt.add_thread(timer_test)
+dt.add_thread(timer_test, 3)
 dt.start(True)
--- Python code/Pi/thread_func.py
+++ Python code/Pi/thread_func.py
@@ -7,36 +7,51 @@
 import time
 from threading import Timer
 
+################################################################################
+
 def print_time(title='default', cycles=3, delay=1):
 	for i in range(cycles):
 		print(title + ": " + time.ctime(time.time()))
 		time.sleep(delay)
-                
+
+################################################################################
+
+def raise_alarm():
+	raise TimeException
+
+###############################################################################
+
 def alarm(count, run):
     print("Process timed out at count %i!") % count
     del run[0] # acts like pass-by-reference, removes the only value in the list
     # essentially turning run into a False boolean.
-                
-def timer_test():
+
+################################################################################
+
+def timer_test(time_to_watch=0):
     count = 0
     run = [1] # allows 'alarm' method to impact this method:
     # Python normally uses pass-by-value, but passing and modifying a list will
     # simulate passing by reference.
-    while run and count < 5:
+    while run and count < 10:
         print("Count: %i") % count
-        timer = Timer(3, alarm, (count, run)) # create a new timer thread:
-        # this is necessary, as a timer thread can only be started once, so we
-        # must create a new thread for each loop through.
-        timer.start() # start the timer
+	if time_to_watch:
+	        timer = Timer(time_to_watch, alarm, (count, run)) # create a new timer thread:
+	        # this is necessary, as a timer thread can only be started once, so we
+	        # must create a new thread for each loop through.
+	        timer.start() # start the timer
         time.sleep(count) # if sleeping longer than 3 seconds, timer should call
         # the 'alarm' method above.
-        timer.cancel() # cancel the timer to prevent false timeouts
+	if time_to_watch:
+	        timer.cancel() # cancel the timer to prevent false timeouts
         print("Count: %i completed.") % count
         if run:
             count += 1
     if not run:
         # if the process is killed by a timeout, success!
         print("Success! Process ended at count " + str(count)) 
-    if count == 5:
+   #  if count == 5:
         # otherwise, failure.
-        print("Count reached 5")
+       #  print("Count reached 5")
+
+#################################################################################
