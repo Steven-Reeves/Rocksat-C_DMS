a9e97e9ce2a593a96f761e056234b717c27569a8
Andy <andyjhorn@gmail.com>
2018-04-08 20:33:46 +0000

    Refactored logic in read_serial.

--- Python code/Pi/serial_func.py
+++ Python code/Pi/serial_func.py
@@ -7,36 +7,46 @@
 
 from threading import Timer
 import serial
+import time
 
 def timeout(port, name):
     print("[Timeout] Device on port {} timeout, flushing port.".format(name))
     port.flush()
     print("[Timeout] Port flushed")
-	#port.write('R')
-	#run[0] += 1
 
 def read_serial(port, baudrate=9600, filename='none', file_type='.txt', wait_time=1):
     num_failures = 0
     if filename == 'none':
         filename = 'serial_in'
-        s = serial.Serial(port, baudrate)
+    s = serial.Serial(port, baudrate, timeout=wait_time*2)
+    time.sleep(.01)
         # Commented out line below for Python 3.5
         #file = open(filename + file_type, 'w')
         # Line below is for Python 3.5:
-        file = open(filename + file_type, 'wb')
-        try:
-            while run[0] < 5:
-                timer = Timer(wait_time, timeout, (run,s))
+    file = open(filename + file_type, 'wb')
+    try:
+        while True:
+            if wait_time > 0:
+                timer = Timer(wait_time, timeout, (s, port))
                 timer.start()
-                buffer = s.readline(timeout=wait_time) # reads bytes of data, default = 1
+            buffer = s.readline() # reads bytes of data, default = 1
+            if wait_time > 0:
                 timer.cancel()
-                # decode if necessary
-                file.write(buffer)
-                print(str(buffer))
-            if run[0] == 5:
+            # decode if necessary
+            if buffer != '':
+                file.write(str(buffer))
+                print("[Buffer] {}".format(str(buffer)))
+            else:
+                print("[read_serial] No input")
+                num_failures += 1
+                if num_failures >= retries:
+                    print("Device {} failed, exiting thread.".format(port))
                     s.close()
                     file.close()
-                    print("Arduino failed 5 times, Exit thread")
-        except KeyboardInterrupt:
-            s.close()
-            file.close()
+                    break
+    except KeyboardInterrupt:
+        s.close()
+        file.close()
+    finally:
+        s.close()
+        file.close()
