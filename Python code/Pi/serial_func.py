8180f7f42922661dbe5af5cd1af20ab80b87773b
Andy <andyhorn@users.noreply.github.com>
2018-04-09 12:25:01 -0700

    Added with statement to serial_func
    The with statement will automatically handle the opening and closing of the data files, hopefully preventing erroneous or empty output.

--- Python code/Pi/serial_func.py
+++ Python code/Pi/serial_func.py
@@ -21,33 +21,34 @@ def read_serial(port, baudrate=9600, filename='none', file_type='.txt', wait_tim
         filename = 'serial_in'
     s = serial.Serial(port, baudrate, timeout=wait_time*2, dsrdtr=True)
     time.sleep(.01)
-    file = open(filename + file_type, 'wb')
-    time.sleep(.01)
-    try:
-        while True:
-            s.flush()
-            if wait_time > 0:
-                timer = Timer(wait_time, timeout, (s, port))
-                timer.start()
+    with open(filename + file_type, 'wb') as file: #can use "with" to automatically close file,
+    # or open and close the file each time the while statement executes. Not closing the file
+    # properly can lead to blank output, especially in Python3.
+        try:
+            while True:
+                s.flush()
+                if wait_time > 0:
+                    timer = Timer(wait_time, timeout, (s, port))
+                    timer.start()
 
-            buffer = s.readline()
-
-            if wait_time > 0:
-                timer.cancel()
-            if buffer != b'':
-                file.write(buffer)
-                print("[{}] {}".format(port, str(buffer)))
-            else:
-                print("[{}] No input".format(port))
-                num_failures += 1
-                if num_failures >= retries:
-                    print("Device [{}] has failed, exiting thread.".format(port))
-                    s.close()
-                    file.close()
-                    break
-    except KeyboardInterrupt:
-        s.close()
-        file.close()
-    finally:
-        s.close()
-        file.close()
+                buffer = s.readline()
+                
+                if wait_time > 0:
+                    timer.cancel()
+                if buffer != b'':
+                    file.write(buffer)
+                    print("[{}] {}".format(port, str(buffer)))
+                else:
+                    print("[{}] No input".format(port))
+                    num_failures += 1
+                    if num_failures >= retries:
+                        print("Device [{}] has failed, exiting thread.".format(port))
+                        s.close()
+                        file.close()
+                        break
+        except KeyboardInterrupt:
+            s.close()
+            file.close()
+        finally:
+            s.close()
+            file.close()
