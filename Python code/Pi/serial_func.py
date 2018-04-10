3e1c6a638b52dddd453e4b6788d05ba79231ae38
Andy <andyjhorn@gmail.com>
2018-04-10 19:01:58 +0000

    Streamlined serial and file open/close with 'with' statements.

--- Python code/Pi/serial_func.py
+++ Python code/Pi/serial_func.py
@@ -1,6 +1,6 @@
-# Author: 	    Andy Horn
-# Date:		    4/4/18
-# Modified: 	4/8/18
+# Author:       Andy Horn
+# Date:	        4/4/18
+# Modified: 	4/10/18
 # Filename:     serial_func.py
 # Overview:     Defines functions to read serial data from a USB port, then print
 #               the data to the screen and write it to a file.
@@ -35,28 +35,29 @@ def read_serial(port, baudrate=9600, filename='none', file_type='.txt', wait_tim
     # or open and close the file each time the while statement executes. Not closing the file
     # properly can lead to blank output, especially in Python3.
         try:
-            while run:
+            with open(filename + file_type, 'a') as file:
+                while run:
 #            s.flush()
-                if wait_time > 0:
-                    timer = Timer(wait_time, read_timeout, (s, port))
-                    timer.start()
+                    if wait_time > 0:
+                        timer = Timer(wait_time, read_timeout, (s, port))
+                        timer.start()
 #            if s.in_waiting() > 0:
-                    buffer = s.readline().decode('ascii')
+                        buffer = s.readline().decode('ascii')
 #            time.sleep(.01)
-                if wait_time > 0:
-                    timer.cancel()
-                if buffer.split():
-                    with open(filename + file_type, 'a') as file:
+                    if wait_time > 0:
+                        timer.cancel()
+                    if buffer.split():
+#                    with open(filename + file_type, 'a') as file:
                         file.write(buffer)
 #                    file.close()
 #                    print("[{}] {}".format(port, str(buffer.decode('ascii'))))
                         print("[{}] {}".format(port, str(buffer)))
-                else:
-                    print("[{}] No input".format(port))
-                    num_failures += 1
-                    if num_failures > retries:
-                        print("Device [{}] has failed, exiting thread.".format(port))
-                        break
+                    else:
+                        print("[{}] No input".format(port))
+                        num_failures += 1
+                        if num_failures > retries:
+                            print("Device [{}] has failed, exiting thread.".format(port))
+                            break
                     #s.close()
                     #file.close()
                     #break
