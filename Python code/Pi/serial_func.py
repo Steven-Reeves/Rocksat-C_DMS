77bf4cbaabeb910651fcedc047910da5306ce6bc
Andy <andyjhorn@gmail.com>
2018-04-10 18:14:42 +0000

    Restored read_serial functionality with proper testing of buffer contents using .split() to return 0 for empty or non-zero for contents.

--- Python code/Pi/serial_func.py
+++ Python code/Pi/serial_func.py
@@ -19,48 +19,49 @@ def read_serial(port, baudrate=9600, filename='none', file_type='.txt', wait_tim
     num_failures = 0
     if filename == 'none':
         filename = 'serial_in'
-    s = serial.Serial(port, baudrate, timeout=wait_time*2, dsrdtr=True)
+    with serial.Serial(port, baudrate, timeout=wait_time*2, dsrdtr=True) as s:
 #    s.open()
 #    time.sleep(.01)
-    s.flush()
-    time.sleep(.02)
+       # s.flush()
+       # time.sleep(.02)
 #    with open(filename + file_type, 'wb') as file: #can use "with" to automatically close file,
     # or open and close the file each time the while statement executes. Not closing the file
     # properly can lead to blank output, especially in Python3.
-    try:
-        while True:
-            s.flush()
-            if wait_time > 0:
-                timer = Timer(wait_time, timeout, (s, port))
-                timer.start()
-
-            buffer = s.readline().decode('ascii')
-            time.sleep(.01)
-            if wait_time > 0:
-                timer.cancel()
-            if buffer.split():
-                with open(filename + file_type, 'a') as file:
-                    file.write(buffer)
+        try:
+            while True:
+#            s.flush()
+                if wait_time > 0:
+                    timer = Timer(wait_time, timeout, (s, port))
+                    timer.start()
+#            if s.in_waiting() > 0:
+                    buffer = s.readline().decode('ascii')
+#            time.sleep(.01)
+                if wait_time > 0:
+                    timer.cancel()
+                if buffer.split():
+                    with open(filename + file_type, 'a') as file:
+                        file.write(buffer)
 #                    file.close()
 #                    print("[{}] {}".format(port, str(buffer.decode('ascii'))))
-                print("[{}] {}".format(port, str(buffer)))
-            else:
-                print("[{}] No input".format(port))
-                num_failures += 1
-                if num_failures > retries:
-                    print("Device [{}] has failed, exiting thread.".format(port))
-                    s.close()
-                    file.close()
-                    break
-    except KeyboardInterrupt:
-        s.close()
-        file.close()
-        print("[read_serial] KeyboardInterrupt")
-    except:
-        s.close()
-        file.close()
-        print("[read_serial] Unhandled Exception")
-    finally:
-        s.close()
-        file.close()
-        print("[read_serial] Exit Code 0")
+                        print("[{}] {}".format(port, str(buffer)))
+                else:
+                    print("[{}] No input".format(port))
+                    num_failures += 1
+                    if num_failures > retries:
+                        print("Device [{}] has failed, exiting thread.".format(port))
+                        break
+                    #s.close()
+                    #file.close()
+                    #break
+        except KeyboardInterrupt:
+        #    s.close()
+        #file.close()
+            print("[read_serial] KeyboardInterrupt")
+        except:
+        #s.close()
+        #file.close()
+            print("[read_serial] Unhandled Exception")
+        finally:
+        #s.close()
+        #file.close()
+            print("[read_serial] Exit Code 0")
