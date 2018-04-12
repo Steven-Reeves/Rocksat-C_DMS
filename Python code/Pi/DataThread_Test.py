722b72906d16d341fc0815aa80956d807890dc8d
Andy Horn <andyjhorn@gmail.com>
2018-04-11 21:07:16 -0700

    Removed join on Watcher.

--- Python code/Pi/DataThread_Test.py
+++ Python code/Pi/DataThread_Test.py
@@ -106,7 +106,7 @@ class DataThread:
                 # Create independent thread to monitor other threads
                 watch = Thread(target=self.__watch_threads)
                 watch.start()
-                watch.join()
+                #watch.join()
             except KeyboardInterrupt:
                 print("[DataThread] KeyboardInterrupt")
                 #raise KeyboardInterrupt
