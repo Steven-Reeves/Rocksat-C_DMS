0779a5a3b7b3fdd208470e44c88e3dfa51466f98
Steven Reeves <steven@digieffects.com>
2018-04-15 15:21:19 -0700

    Added updated Data_thread and main.py

--- Python code/Pi/DataThread.py
+++ Python code/Pi/DataThread.py
@@ -1,92 +1,128 @@
 # Author:       Andy Horn
 # Date:         4/4/2018
-# Modified:     4/8/2018
+# Modified:     4/15/2018
 # Filename:     DataThread.py
 # Overview:     DataThread class that manages simultaneous threads
 
-from threading import Thread
-import time
+from threading import Thread, Timer
+import time, serial
 
 class DataThread:
 
     def __init__(self):
         self.__threads = []
+        self.run_list = []
         self.num_threads = 0
         self.started = False
+        self.run = False
 
-    @staticmethod
+    def main_timer_complete(self, thread_id):
+        self.run_list[thread_id] = False
+        print("[Thread {} timer] Countdown complete.".format(thread_id))
+
+    def serial_read_timeout(self, port, name):
+        print("[Timeout] Device [{}] is unresponsive, flushing port.".format(name))
+        port.flush()
+        print("[Timeout] Port flushed")
+
     def __watch_threads(self):
+        print("[Watcher] Activated")
         start_time = time.time()
         while self.__threads:
+           # print("[Watcher] Watching")
             for t in self.__threads:
                 if not t.isAlive():
                     self.__threads.remove(t)
                     self.num_threads -= 1
+            time.sleep(2)
         if not self.__threads:
             print("[Watcher] All threads complete.")
             print("[Watcher] Time to complete: " + str("%.2f" % (time.time() - start_time)))
             self.num_threads = 0
             self.started = False
 
-    #staticmethod
-    def read_serial(port, baudrate=9600, filename='none', file_type='.txt', wait_time=2, retries=1):
+    def read_serial(self, thread_id, port, baudrate=9600, filename='none', file_type='.txt', wait_time=2, retries=1):
         num_failures = 0
-        run = ['1']
-        countdown = Timer(60, mainTimerComplete, (run,))
+        countdown = Timer(60, self.main_timer_complete, (thread_id,))
         countdown.start()
         start_time = time.time()
         if filename == 'none':
             filename = port
         try:
             with serial.Serial(port, baudrate, timeout=wait_time * 2, dsrdtr=True) as s:
-                with open(filename + file_type, 'a') as file:
-                    while run:
+                s.flush()
+                with open(filename + file_type, 'ab') as file:
+                    while self.run_list[thread_id]:
                         if wait_time > 0:
-                            timer = Timer(wait_time, serialReadTimeout, (s, port))
+                            timer = Timer(wait_time, self.serial_read_timeout, (s, port))
                             timer.start()
                         # buffer = s.readline().decode('ascii')
                         buffer = s.readline()
-                        s.flush()
+                        #s.flush()
                         if wait_time > 0:
                             timer.cancel()
                         if buffer.split():
-                            file.write(str(time.time() - start_time) + str(buffer))
-                            print("[{}] {}".format(port, str(buffer)))
+                            time_lapsed = time.time() - start_time
+                            #file.write(str(time_lapsed))
+                            file.write(buffer)
+                            print("[{}] {} {}".format(port, str(time_lapsed), str(buffer)))
                         else:
                             print("[{}] No input".format(port))
                             num_failures += 1
                             if num_failures > retries:
                                 print("Device [{}] has failed, exiting thread.".format(port))
-                                del run[0]
+                                self.run_list[thread_id] = False
                                 # break
         except KeyboardInterrupt:
             print("[read_serial] KeyboardInterrupt")
+            self.run_list[thread_id] = False
+            countdown.cancel()
         # except:
         # print("[read_serial] Unhandled Exception")
         finally:
             print("[read_serial] Exit Code 0")
+            countdown.cancel()
 
     # *vals will take any remaining values as a tuple
-    def add_thread(self, func, *vals):
+    def add_thread(self, *values):
+        arg_list = list(values)
+        insert = [self.num_threads]
+        arg_list[:0] = insert
+        new_values = tuple(arg_list)
         if not self.started:
-            t = Thread(target=func, args=vals)
+            t = Thread(target=self.read_serial, args=new_values)
             self.__threads.append(t)
+            self.run_list.append(False)
             self.num_threads += 1
         else:
             print("[add_thread] Cannot add threads while running.")
 
     def start(self):
-        try:
-            for t in self.__threads:
-                #t.daemon = True
-                t.start()
-            self.started = True
+        if self.__threads:
+            index = 0
+            self.run = True
+            try:
+                for t in self.__threads:
+                    self.run_list[index] = True
+                    t.start()
+                    index += 1
+                self.started = True
                 # Create independent thread to monitor other threads
-            watch =  Thread(target=self.__watch_threads, args=(self,))
-            watch.start()
-            watch.join()
-        except KeyboardInterrupt:
-            print("[DataThread] KeyboardInterrupt")
-            raise KeyboardInterrupt
-        finally:
-            print("[DataThread] All threads complete.")
+                watch = Thread(target=self.__watch_threads)
+                #watch.daemon = True
+                watch.start()
+                watch.join()
+            except KeyboardInterrupt:
+                print("[DataThread] KeyboardInterrupt")
+                #raise KeyboardInterrupt
+                self.stop()
+                print("[DataThread] Stop() called")
+            finally:
+                print("[DataThread] All threads complete.")
+
+    def stop(self):
+        print("[Stop] Call received.")
+        self.run = False
+        for n in range(len(self.run_list)):
+            self.run_list[n] = False
+            print("[DataThread] Flag set to false.")
--- Python code/Pi/main.py
+++ Python code/Pi/main.py
@@ -1,19 +1,21 @@
 # Goal: Read serial data from multiple ports through multiple threads.
 # Author:	Andy Horn
 # Date:		4/4/2018
-# Modified:	4/10/2018
+# Modified:	4/15/2018
 
 from DataThread import DataThread
-from serial_func import read_serial
+
 
 try:
     dt = DataThread()
-    dt.add_thread(read_serial, "/dev/ttyACM0", 57600, "Test-1")
-    dt.add_thread(read_serial, "/dev/ttyACM1", 57600, "Test-2")
+    # Tests for USB hub local to Andy's pi
+    dt.add_thread("/dev/ttyACM0", 57600, "Test-1")
+    dt.add_thread("/dev/ttyACM1", 57600, "Test-2")
 	# Tests for USB hub local to Steven's pi
-	#dt.add_thread(read_serial, "/dev/ttyUSB0", 9600, "Test-2")
-	#dt.add_thread(read_serial, "/dev/ttyUSB1", 9600, "Test-3")
-	#dt.add_thread(read_serial, "/dev/ttyUSB2", 9600, "Test-4")
+    #dt.add_thread("/dev/ttyACM0", 57600, "Test-1")
+	#dt.add_thread("/dev/ttyUSB0", 9600, "Test-2")
+	#dt.add_thread("/dev/ttyUSB1", 9600, "Test-3")
+	#dt.add_thread("/dev/ttyUSB2", 9600, "Test-4")
     dt.start()
 except KeyboardInterrupt:
     print("Exit main")
