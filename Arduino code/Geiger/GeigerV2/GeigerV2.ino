8cbef414cf9097eebcc9836c60bec9ba26714dc5
Andy Horn <andyjhorn@gmail.com>
2018-05-26 22:30:56 -0700

    Code cleaned and refactored.
    Refactored code to use less memory; got rid of unnecessary variables; moved repeated code into functions.
    Added lots of comments to clarify logic and processes.

--- Arduino code/Geiger/GeigerV2/GeigerV2.ino
+++ Arduino code/Geiger/GeigerV2/GeigerV2.ino
@@ -3,12 +3,12 @@
 
 /************* VARIABLE SET UP *************/
 
-const short chipSelect = 10;      // Actual pin 10
-const short tubeOneIntPin = 3;    // unshielded
-const short tubeTwoIntPin = 2;    // FE3O4
-const short tubeThreeIntPin = 7;  // SRCO3
-const short tubeFourIntPin = 0;   // WAX
-
+#define BAUDRATE          57600 // Serial baudrate
+#define chipSelect        10    // Chip select pin for SD card
+#define tubeOneIntPin     3     // Unshielded tube
+#define tubeTwoIntPin     2     // FE3O4 shielded tube
+#define tubeThreeIntPin   7     // SRCO3 shielded tube
+#define tubeFourIntPin    0     // Wax shielded tube
 
 unsigned int tubeOneHits = 0;     // Used to count intermediate hits
 unsigned long tubeOneTotal = 0;   // Used to count total number of hits
@@ -21,13 +21,7 @@ unsigned long tubeFourTotal = 0;  // Used to count total number of hits
 
 bool sdEnable = false;      // Flag set to true when SD card successfully initialized
 bool serialEnable = false;  // Flag set to true when Serial port successfully opened
-bool debug = false;         // Flag set to true for debugging -> produces more serial statements
-
-unsigned long timestamp = 0;  // Used to hold timestamp value (ms since boot)
-
-String printOut = ""; // Used to hold string to be printed to Serial and/or written to SD card
-
-
+bool debug = false;         // Set flag to true for debugging -> produces more serial statements
 
 void setup() 
 {
@@ -37,127 +31,102 @@ void setup()
   // print to serial or write to the SD check for the flag before executing,
   // eliminating errors when either one isn't functioning correctly.
   
-  InitSerial(); // Initialize serial output, used to send data to Raspberry Pi
-  InitSd();     // Initialize SD card for local data storage
+  InitSerial(); // Initialize serial output, used to send data to Raspberry Pi.
+  InitSd();     // Initialize SD card for local data storage.
+  
   /********************************/
 
   /******** INTERRUPT SETUP ********/
   // Interrupt pins must be set with the internal pullup resistors activated, otherwise
-  // the interrupts are triggered by electrical noise, nullifying all results.
+  // the interrupts are triggered by electrical noise, effectively nullifying all results.
   pinMode(tubeOneIntPin, INPUT_PULLUP);
   pinMode(tubeTwoIntPin, INPUT_PULLUP);
   pinMode(tubeThreeIntPin, INPUT_PULLUP);
   pinMode(tubeFourIntPin, INPUT_PULLUP);
-  
+
+  // Call interrupt methods on RISING edge of signal.
   attachInterrupt(digitalPinToInterrupt(tubeOneIntPin), TubeOne, RISING);
   attachInterrupt(digitalPinToInterrupt(tubeTwoIntPin), TubeTwo, RISING);
   attachInterrupt(digitalPinToInterrupt(tubeThreeIntPin), TubeThree, RISING);
   attachInterrupt(digitalPinToInterrupt(tubeFourIntPin), TubeFour, RISING);
   /*********************************/
 
-  // debug printout
+  // Debug printout.
   if (serialEnable && debug)
   {
     Serial.println("Setup complete!");  
   }
 }
 
-
 void loop() {
+  
+  // Main loop of program will run anytime there are no interrupt methods on
+  // the call stack. It will check each tube's intermediate hit counter and 
+  // print the appropriate information when there are changes.
+    
   if (tubeOneHits > 0)
   {
-    tubeOneTotal += tubeOneHits;
-    tubeOneHits = 0;
-    timestamp = millis();
-    printOut = String(timestamp) + ",unshielded," + String(tubeOneTotal);
-    if (serialEnable)
-    {
-      PrintToSerial(printOut);  
-    }
-    if (sdEnable)
-    {
-      WriteToSD(printOut);
-    } 
+    UpdateCount(tubeOneTotal, tubeOneHits, "unshielded");
   }
   if (tubeTwoHits > 0)
   {
-    tubeTwoTotal += tubeTwoHits;
-    tubeTwoHits = 0;
-    timestamp = millis();
-    printOut = String(timestamp) + ",fe3o4," + String(tubeTwoTotal);
-    if (serialEnable)
-    {
-      PrintToSerial(printOut);  
-    }
-    if (sdEnable)
-    {
-      WriteToSD(printOut);
-    }
+    UpdateCount(tubeTwoTotal, tubeTwoHits, "fe3o4");
   }
   if (tubeThreeHits > 0)
   {
-    tubeThreeTotal += tubeThreeHits;
-    tubeThreeHits = 0;
-    timestamp = millis();
-    printOut = String(timestamp) + ",srco3," + String(tubeThreeTotal);
-    if (serialEnable)
-    {
-      PrintToSerial(printOut);  
-    }
-    if (sdEnable)
-    {
-      WriteToSD(printOut);
-    }
+    UpdateCount(tubeThreeTotal, tubeThreeHits, "src03");
   }
   if (tubeFourHits > 0)
   {
-    tubeFourTotal += tubeFourHits;
-    tubeFourHits = 0;
-    timestamp = millis();
-    printOut = String(timestamp) + ",wax," + String(tubeFourTotal);
-    if (serialEnable)
-    {
-      PrintToSerial(printOut);  
-    }
-    if (sdEnable)
-    {
-      WriteToSD(printOut);
-    }
-    //UpdateCount(tubeFourTotal, tubeFourHits, "wax");
+    UpdateCount(tubeFourTotal, tubeFourHits, "wax");
   }
 }
 
 /************* OUTPUT FUNCTIONS *************/
 
-void UpdateCount(unsigned long &totalHits, unsigned long &newHits, String shielding)
+void UpdateCount(unsigned long & totalHits, unsigned int & newHits, String shielding)
 {
-  totalHits += newHits;
-  newHits = 0;
-  timestamp = millis();
-  printOut = String(millis()) + "," + shielding + "," + String(totalHits);
+  // Counter variables are passed by reference in order to update/manipulate the global variables.
+  
+  totalHits += newHits; // Update total hit counter with new additions.
+  newHits = 0;          // Reset the intermediate counter to zero.
+
+  // Build output string:
+  String printOut = String(millis()) + "," + shielding + "," + String(totalHits);
+  
   if (serialEnable)
   {
+    // Ff serial port is open, print the message to serial.
     PrintToSerial(printOut);
   }
   if (sdEnable)
   {
+    // If the SD card is initialized, write the message to the SD card.
     WriteToSD(printOut);
   }
 }
+
 /************* PRINT FUNCTIONS *************/
-void PrintToSerial(String printMe)
+
+// Write string to serial port
+void PrintToSerial(String &printMe)
 {
   if (serialEnable)
   {
+    // If the serial port is open, print the message.
+    // This should be checked before calling this method,
+    // but to be safe it is checked here as well.
     Serial.println(printMe); 
   }
 }
 
-void WriteToSD(String writeMe)
+// Write string to SD card
+void WriteToSD(String &writeMe)
 {
-  File dataFile = SD.open("geiger.txt", FILE_WRITE);
+  File dataFile = SD.open("geiger.txt", FILE_WRITE); // open data file
   if (dataFile)
   {
+    // If the file is open, write data and close the file.
     dataFile.println(writeMe);
     dataFile.close();
   }
@@ -165,78 +134,66 @@ void WriteToSD(String writeMe)
   {
     if (serialEnable && debug)
     {
+      // If the serial port is open and debug mode is on, print a failure message.
       Serial.println("Unable to write to SD card.");  
     }
   }
 }
 
-
 /************* INITIALIZATION FUNCTIONS *************/
+
+// Initialize SD card
 void InitSd()
 {
   short i = 0;
-  while (!SD.begin(chipSelect) && i < 100)
+  while (!SD.begin(chipSelect) && i++ < 100) { delay(10); } // Try to initialize SD card for up to 1 second.
+  
+  if (SD.begin(chipSelect))
   {
-    i++;
-    delay(10);
-    // try to initialize card before giving up
-  }
-  if (!SD.begin(chipSelect))
-  {
+    // If the SD card is initialized, set the flag appropriately.
+    sdEnable = true;
+    
     if (serialEnable && debug)
     {
-      Serial.println("Failed to initialize SD card.");  
+      // If the serial port is open and debug mode is on, print the SD card status.
+      Serial.println("SD card initialized successfully.");
     }
   }
   else
   {
     if (serialEnable && debug)
     {
-      Serial.println("SD Card initialized!");  
+      // If the serial port is open and debug mode is on, print a failure message.
+      Serial.println("Failed to initialize SD card.");  
     }
-    sdEnable = true;
   }
 }
 
+// Initializes the serial port
 void InitSerial()
 {
   short i = 0;
-  Serial.begin(57600);
-  while (!Serial && i < 100)
-  {
-    i++;
-    delay(10);
-    // try to connect for 1 second before giving up
-  }
+  Serial.begin(BAUDRATE);
+  while (!Serial && i++ < 100) { delay(10); } // Try to open the serial port for up to 1 second.
+
   if(Serial)
   {
+    // If the port opened successfully, set the flag appropriately.
     serialEnable = true;
     if (debug)
     {
+      // If debug mode is on, print the serial status.
       Serial.println("Serial open! serialEnable set to true");
     }
   }
 }
 
-
 /************* INTERRUPT FUNCTIONS *************/
 
-void TubeOne()
-{
-  tubeOneHits++;
-}
+void TubeOne() { tubeOneHits++; }
 
-void TubeTwo()
-{
-  tubeTwoHits++;
-}
+void TubeTwo() { tubeTwoHits++; }
 
-void TubeThree()
-{
-  tubeThreeHits++;
-}
+void TubeThree() { tubeThreeHits++; }
 
-void TubeFour()
-{
-  tubeFourHits++;
-}
+void TubeFour() { tubeFourHits++; }
